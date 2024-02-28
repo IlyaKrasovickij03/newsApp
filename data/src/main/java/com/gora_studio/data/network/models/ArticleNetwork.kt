eedcb52830c936eb72a27d6267dd6d0a0010ca6d
Ilya Krasovickij <145861478+IlyaKrasovickij03@users.noreply.github.com>
2024-02-28 21:07:51 +0300

    fixed

--- data/src/main/java/com/gora_studio/data/network/models/ArticleNetwork.kt
+++ data/src/main/java/com/gora_studio/data/network/models/ArticleNetwork.kt
@@ -4,12 +4,12 @@ import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class Article(
+data class ArticleNetwork(
     @SerialName("author") val author: String,
     @SerialName("content") val content: String,
     @SerialName("description") val description: String,
     @SerialName("publishedAt") val publishedAt: String,
-    @SerialName("source") val source: Source,
+    @SerialName("source") val source: SourceNetwork,
     @SerialName("title") val title: String,
     @SerialName("url") val url: String,
     @SerialName("urlToImage") val urlToImage: String
